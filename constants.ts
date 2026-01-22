3f6ef3e17a7dc1e679196a36f2fac6b4d931372a
Tim Trainor <28212433+timjtrainor@users.noreply.github.com>
2026-01-21 20:54:46 -0800

    Updated the link to the Requirements Evaluator (EVALs) example

--- constants.ts
+++ constants.ts
@@ -24,7 +24,7 @@ export const POC_DATA: Project[] = [
     summary: 'A serverless AI platform built with AWS Lambda and Amazon Bedrock that systematically audits software requirements for precision and quality. It streamlines the technical review process by providing automated scoring, actionable improvement suggestions, and a dedicated performance harness to ensure high-accuracy AI outputs.',
     category: 'POC',
     tags: ['AWS', 'Generative AI', 'EVALs', 'SDLC',],
-    link: 'https://req-eval.trainoronline.com/'
+    link: 'https://req-eval.the-trainors.com/'
   },
 ];
 
